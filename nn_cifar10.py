486ce4e3e96d37b8f2c308aecfc0e63743a4e928
lazy-codebear <tonys65536@gmail.com>
2022-11-10 10:32:03 +0800

    using sequential

--- nn_cifar10.py
+++ nn_cifar10.py
@@ -5,24 +5,37 @@ from torch import nn
 class CNN(nn.Module):
     def __int__(self):
         super(CNN, self).__int__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2)
-        self.maxpool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2)
-        self.maxpool2 = nn.MaxPool2d(2)
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
-        self.maxpool3 = nn.MaxPool2d(2)
-        self.flatten = nn.Flatten()
-        self.linear1 = nn.Linear(in_features=1024, out_features=64)
-        self.linear2 = nn.Linear(in_features=64, out_features=10)
+        # self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5, padding=2)
+        # self.maxpool1 = nn.MaxPool2d(2)
+        # self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, padding=2)
+        # self.maxpool2 = nn.MaxPool2d(2)
+        # self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5, padding=2)
+        # self.maxpool3 = nn.MaxPool2d(2)
+        # self.flatten = nn.Flatten()
+        # self.linear1 = nn.Linear(in_features=1024, out_features=64)
+        # self.linear2 = nn.Linear(in_features=64, out_features=10)
+
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(1024, 64),
+            nn.Linear(64, 10)
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.maxpool2(x)
-        x = self.conv3(x)
-        x = self.maxpool3(x)
-        x = self.flatten(x)
-        x = self.linear1(x)
-        x = self.linear2(x)
+        # x = self.conv1(x)
+        # x = self.maxpool1(x)
+        # x = self.conv2(x)
+        # x = self.maxpool2(x)
+        # x = self.conv3(x)
+        # x = self.maxpool3(x)
+        # x = self.flatten(x)
+        # x = self.linear1(x)
+        # x = self.linear2(x)
+        x = self.model(x)
         return x
